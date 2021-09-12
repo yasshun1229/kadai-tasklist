dd3286e04657a9eca38e212c6f349bc7d3ba67db
yasuda.shunsuke <yasshun1229.com>
2021-09-12 09:31:27 +0000

    kadai-tasklist

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,17 @@
+class TasksController < ApplicationController
+  def index
+    @tasks = Task.all
+  end
+  
+  def show 
+    @task = Task.find(params[:id])
+  end
+  
+  def new
+    @task = Task.new
+  end
+  
+  def edit
+    @task = Task.find(params[:id])
+  end
+end

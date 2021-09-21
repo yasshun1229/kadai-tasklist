0df7ea419b3f9b889c5c398b28e92d428097c06a
yasuda.shunsuke <yasshun1229.com>
2021-09-21 10:52:50 +0000

    ユーザ登録機能完了

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,11 +4,29 @@ class UsersController < ApplicationController
   end
 
   def show
+    @user = User.find(params[:id])
   end
 
   def new
+    @user = User.new
   end
 
   def create
+    @user = User.new(user_params)
+    
+    if @user.save
+      flash[:success] = "ユーザーを登録しました"
+      redirect_to @user
+    else
+      flash.now[:danger] = "ユーザーの登録に失敗しました"
+      render :new
+    end
+  end
+  
+  # Strong Paramter
+  private
+  
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   root to: "tasks#index"
   
   get "signup", to: "users#new"
-  resources :tasks, only: [:index, :show, :create, :new, :edit] 
+  resources :tasks, :users, only: [:index, :show, :create, :new, :edit]
 end
 
 # 5行目の"tasks"は"users"かもしれん。

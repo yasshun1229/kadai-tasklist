b1e53e5a52743e03c93044560baff37a2409b2f9
yasuda.shunsuke <yasshun1229.com>
2021-09-23 12:07:09 +0000

    kadaitasklist.app

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,5 +1,4 @@
 class Task < ApplicationRecord
   validates :content, presence: true, length: { maximum: 255 }
   validates :status, presence: true, length: { maximum: 10 }
-  user_id :content, presence: true, length: { maximum: 255 }
 end
